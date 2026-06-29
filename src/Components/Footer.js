1f8358e4e09c2064edd723226979bfa9acd9d79a
Gourav Pal <gouravsir99@gmail.com>
2026-06-29 14:14:27 +0530

    Updated Publicly

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,8 +4,11 @@ import React from "react";
 const Footer = () => {
   return (
     <>
-    <footer className="bg-dark text-white text-center p-3 mt-5">
-      © 2026 NewsApp | Made with React ❤️
+    <footer className="bg-dark text-white text-center p-3 mt-5 fixed">
+      <div>© 2026 Publicly-NewsApp | Made Using React By Sourav Pal</div>
+      <pre className="mt-2">
+      <a href="https://github.com/souravpal11" className="text-reset text-decoration-none fs-6"><i className="fa-brands fa-github m-2"></i>souravpal11</a> | <a href="https://www.linkedin.com/in/sourav-pal-4195aa380/" className="text-reset text-decoration-none fs-6"><i className="fa-brands fa-linkedin m-2"></i>SOURAV PAL</a>
+      </pre>
     </footer>
     </>
   );
