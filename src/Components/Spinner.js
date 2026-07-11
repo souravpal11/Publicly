2ea334a7038e55cad0d59efcabadc0121a2805d4
Gourav Pal <gouravsir99@gmail.com>
2026-07-11 07:26:47 +0530

    fix

--- src/Components/Spinner.js
+++ src/Components/Spinner.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import loading from "../assets/loading.json"
+import Lottie from 'lottie-react'
 
 const Spinner = () => {
   return (
     <div className="text-center d-flex align-item-center justify-content-center">
-      <div className="spinner-border text-dark" role="status">
-      </div>
+      {/* <div className="spinner-border text-dark mt-2" role="status">
+      </div> */}
+      <Lottie animationData={loading} loop={true} style={{height:"100px",width:"100px"}}/>
+
     </div>
   );
 };
